8bad61aead2a095d8c7807ddfdc4ad0ab5fd53c4
Jason Wong <jawong@proofpoint.com>
2017-03-24 10:48:04 -0400

    Changed the hello universe class to be more descriptive with the comments.

---
+++ src/HelloUniverse.java
@@ -0,0 +1,14 @@
+
+/**
+ * A class that will output the string "Hello Universe" which is better than
+ * "Hello World".
+ * 
+ * @author Jason Wong
+ *
+ */
+public class HelloUniverse {
+
+    public static void main(String[] args) {
+	System.out.println("Hello Universe");
+    }
+}
